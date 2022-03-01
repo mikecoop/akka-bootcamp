77a452107353767e0d79c38b580764d24fdc4083
Mike Cooper <mikecoop@users.noreply.github.com>
2022-02-28 21:10:53 -0500

    Finish Lesson 2

--- src/Unit-1/FSharp/DoThis/Actors.fs
+++ src/Unit-1/FSharp/DoThis/Actors.fs
@@ -3,40 +3,70 @@ namespace WinTail
 open System
 open Akka.Actor
 open Akka.FSharp
+open WinTail.Messages
 
 module Actors =
-    type Command = 
-    | Start
-    | Continue
-    | Message of string
-    | Exit
+    type Command =
+        | Start
+        | Continue
+        | Message of string
+        | Exit
 
-    let (|Message|Exit|) (str:string) =
+    let (|Message|Exit|) (str: string) =
         match str.ToLower() with
         | "exit" -> Exit
         | _ -> Message(str)
 
+    // Active pattern matching to determine the characteristics of the message (empty, even length, or odd length).
+    let (|EmptyMessage|MessageLengthIsEven|MessageLengthIsOdd|) (msg: string) =
+        match msg.Length, msg.Length % 2 with
+        | 0, _ -> EmptyMessage
+        | _, 0 -> MessageLengthIsEven
+        | _, _ -> MessageLengthIsOdd
 
-    let consoleReaderActor (consoleWriter: IActorRef) (mailbox: Actor<_>) message = 
-        let line = Console.ReadLine ()
-        match line with
-        | Exit -> mailbox.Context.System.Terminate() |> ignore
-        | Message(input) -> 
-            // send input to the console writer to process and print
-            consoleWriter <! input
+    // Print instructions to the console
+    let doPrintInstructions () =
+        Console.WriteLine "Write whatever you want into the console!"
+        Console.WriteLine "Some entries will pass validation, and some won't...\n\n"
+        Console.WriteLine "Type 'exit' to quit this application at any time.\n"
 
-            // continue reading messages from the console
-            mailbox.Self <! Continue
+    let consoleReaderActor (consoleWriter: IActorRef) (mailbox: Actor<_>) message =
+        match box message with
+        | :? Command as command ->
+            match command with
+            | Start -> doPrintInstructions ()
+            | _ -> ()
+        | :? InputResult as inputResult ->
+            match inputResult with
+            | InputError _ as error -> consoleWriter <! error
+            | _ -> ()
+        | _ -> ()
 
-    let consoleWriterActor message = 
-        let (|Even|Odd|) n = if n % 2 = 0 then Even else Odd
-    
+        let getAndValidateInput () =
+            let line = Console.ReadLine()
+            match line with
+            | Exit -> mailbox.Context.System.Terminate () |> ignore
+            | Message(input) ->
+                match input with
+                | EmptyMessage ->
+                    mailbox.Self <! InputError ("No input received.", ErrorType.Null)
+                | MessageLengthIsEven ->
+                    consoleWriter <! InputSuccess ("Thank you! The message was valid.")
+                    mailbox.Self <! Continue
+                | _ ->
+                    mailbox.Self <! InputError ("The message is invalid (odd number of characters)!", ErrorType.Validation)
+
+        getAndValidateInput ()
+
+    let consoleWriterActor message =
         let printInColor color message =
             Console.ForegroundColor <- color
-            Console.WriteLine (message.ToString ())
-            Console.ResetColor ()
+            Console.WriteLine(message.ToString())
+            Console.ResetColor()
 
-        match message.ToString().Length with
-        | 0    -> printInColor ConsoleColor.DarkYellow "Please provide an input.\n"
-        | Even -> printInColor ConsoleColor.Red "Your string had an even # of characters.\n"
-        | Odd  -> printInColor ConsoleColor.Green "Your string had an odd # of characters.\n"
+        match box message with
+        | :? InputResult as inputResult ->
+            match inputResult with
+            | InputError (reason, _) -> printInColor ConsoleColor.Red reason
+            | InputSuccess reason -> printInColor ConsoleColor.Green reason
+        | _ -> printInColor ConsoleColor.Yellow (message.ToString ())
---
+++ src/Unit-1/FSharp/DoThis/Mesages.fs
@@ -0,0 +1,12 @@
+﻿namespace WinTail
+
+module Messages =
+    // The user didn't define any input or the input was not valid.
+    type ErrorType =
+    | Null
+    | Validation
+
+    // Discriminated union to determine whether or not the user input was valid.
+    type InputResult =
+    | InputSuccess of string
+    | InputError of reason: string * errorType: ErrorType
