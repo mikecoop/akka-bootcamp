b43cf92e1e4e7017e284556803f1aa1c4e9a8c07
Mike Cooper <mikecoop@users.noreply.github.com>
2022-03-02 22:22:57 -0500

    Finish Lesson 3

--- src/Unit-1/FSharp/DoThis/Actors.fs
+++ src/Unit-1/FSharp/DoThis/Actors.fs
@@ -12,51 +12,47 @@ module Actors =
         | Message of string
         | Exit
 
-    let (|Message|Exit|) (str: string) =
-        match str.ToLower() with
-        | "exit" -> Exit
-        | _ -> Message(str)
+    let consoleReaderActor (validation: IActorRef) (mailbox: Actor<_>) message =
+        // Print instructions to the console
+        let doPrintInstructions () =
+            Console.WriteLine "Write whatever you want into the console!"
+            Console.WriteLine "Some entries will pass validation, and some won't...\n\n"
+            Console.WriteLine "Type 'exit' to quit this application at any time.\n"
 
-    // Active pattern matching to determine the characteristics of the message (empty, even length, or odd length).
-    let (|EmptyMessage|MessageLengthIsEven|MessageLengthIsOdd|) (msg: string) =
-        match msg.Length, msg.Length % 2 with
-        | 0, _ -> EmptyMessage
-        | _, 0 -> MessageLengthIsEven
-        | _, _ -> MessageLengthIsOdd
-
-    // Print instructions to the console
-    let doPrintInstructions () =
-        Console.WriteLine "Write whatever you want into the console!"
-        Console.WriteLine "Some entries will pass validation, and some won't...\n\n"
-        Console.WriteLine "Type 'exit' to quit this application at any time.\n"
-
-    let consoleReaderActor (consoleWriter: IActorRef) (mailbox: Actor<_>) message =
-        match box message with
-        | :? Command as command ->
-            match command with
-            | Start -> doPrintInstructions ()
-            | _ -> ()
-        | :? InputResult as inputResult ->
-            match inputResult with
-            | InputError _ as error -> consoleWriter <! error
-            | _ -> ()
-        | _ -> ()
+        let (|Message|Exit|) (str: string) =
+            match str.ToLower() with
+            | "exit" -> Exit
+            | _ -> Message(str)
 
         let getAndValidateInput () =
             let line = Console.ReadLine()
             match line with
             | Exit -> mailbox.Context.System.Terminate () |> ignore
-            | Message(input) ->
-                match input with
-                | EmptyMessage ->
-                    mailbox.Self <! InputError ("No input received.", ErrorType.Null)
-                | MessageLengthIsEven ->
-                    consoleWriter <! InputSuccess ("Thank you! The message was valid.")
-                    mailbox.Self <! Continue
-                | _ ->
-                    mailbox.Self <! InputError ("The message is invalid (odd number of characters)!", ErrorType.Validation)
+            | _ -> validation <! line
+
+        match box message with
+        | :? Command as command ->
+            match command with
+            | Start -> doPrintInstructions ()
+            | _ -> ()
+        | _ -> ()
 
         getAndValidateInput ()
+
+    let validationActor (consoleWriter: IActorRef) (mailbox: Actor<_>) message =
+        // Active pattern matching to determine the characteristics of the message (empty, even length, or odd length).
+        let (|EmptyMessage|MessageLengthIsEven|MessageLengthIsOdd|) (msg: string) =
+            match msg.Length, msg.Length % 2 with
+            | 0, _ -> EmptyMessage
+            | _, 0 -> MessageLengthIsEven
+            | _, _ -> MessageLengthIsOdd
+
+        match message with
+        | EmptyMessage -> consoleWriter <! InputError ("No input received.", ErrorType.Null)
+        | MessageLengthIsEven -> consoleWriter <! InputSuccess ("Thank you! The message was valid.")
+        | _ -> consoleWriter <! InputError ("The message is invalid (odd number of characters)!", ErrorType.Validation)
+
+        mailbox.Sender() <! Continue
 
     let consoleWriterActor message =
         let printInColor color message =
--- src/Unit-1/FSharp/DoThis/Program.fs
+++ src/Unit-1/FSharp/DoThis/Program.fs
@@ -8,7 +8,8 @@ let main argv =
     // initialize an actor system
     let myActorSystem = System.create "MyActorSystem" (Configuration.load ())
     let consoleWriterActor = spawn myActorSystem "consoleWriterActor" (actorOf Actors.consoleWriterActor)
-    let consoleReaderActor = spawn myActorSystem "consoleReaderActor" (actorOf2 (Actors.consoleReaderActor consoleWriterActor))
+    let validationActor = spawn myActorSystem "validationActor" (actorOf2 (Actors.validationActor consoleWriterActor))
+    let consoleReaderActor = spawn myActorSystem "consoleReaderActor" (actorOf2 (Actors.consoleReaderActor validationActor))
 
     // tell the consoleReader actor to begin
     consoleReaderActor <! Start
