6492fa9af7824ed56a2d3779a6f9a9092212ac71
Mike Cooper <mikecoop@users.noreply.github.com>
2022-03-08 15:57:07 -0500

    Finish Lesson 3

--- src/Unit-2/DoThis/Form.fs
+++ src/Unit-2/DoThis/Form.fs
@@ -9,7 +9,9 @@ open Akka.FSharp
 open System.Drawing
 open System.Windows.Forms
 open System.Windows.Forms.DataVisualization.Charting
+open Akka.FSharp.Spawn
 open Akka.Util.Internal
+open ChartApp
 
 [<AutoOpen>]
 module Form =
@@ -17,28 +19,41 @@ module Form =
     let form = new Form(Name = "Main", Visible = true, Text = "System Metrics", AutoScaleDimensions = SizeF(6.F, 13.F), AutoScaleMode = AutoScaleMode.Font, ClientSize = Size(684, 446))
     let chartArea1 = new ChartArea(Name = "ChartArea1")
     let legend1 = new Legend(Name = "Legend1")
-    let series1 = new Series(Name = "Series1", ChartArea = "ChartArea1", Legend = "Legend1")
 
-    let btnAddSeries = new Button(Name = "btnAddSeries", Text = "Add Series", Location = Point(540, 300), Size = Size(100, 40), TabIndex = 1, UseVisualStyleBackColor = true)
+    // create the buttons
+    let btnCpu = new Button(Name = "btnCpu", Text = "CPU (ON)", Location = Point(560, 275), Size = Size(110, 40), TabIndex = 1, UseVisualStyleBackColor = true)
+    let btnMemory = new Button(Name = "btnMemory", Text = "MEMORY (OFF)", Location = Point(560, 320), Size = Size(110, 40), TabIndex = 2, UseVisualStyleBackColor = true)
+    let btnDisk = new Button(Name = "btnDisk", Text = "DISK (OFF)", Location = Point(560, 365), Size = Size(110, 40), TabIndex = 3, UseVisualStyleBackColor = true)
 
     sysChart.BeginInit ()
     form.SuspendLayout ()
     sysChart.ChartAreas.Add chartArea1
     sysChart.Legends.Add legend1
-    sysChart.Series.Add series1
-    form.Controls.Add btnAddSeries
+
+    // add buttons to the form
+    form.Controls.Add btnCpu
+    form.Controls.Add btnMemory
+    form.Controls.Add btnDisk
+
     form.Controls.Add sysChart
+
     sysChart.EndInit ()
     form.ResumeLayout false
 
     let load (myActorSystem:ActorSystem) =
         let chartActor = spawn myActorSystem "charting" (Actors.chartingActor sysChart)
-        let series = ChartDataHelper.randomSeries "FakeSeries1" None None
-        chartActor <! InitializeChart(Map.ofList [(series.Name, series)])
+        let coordinatorActor = spawn myActorSystem "counters" (Actors.performanceCounterCoordinatorActor chartActor)
+        let toggleActors = Map.ofList [
+            (CounterType.Cpu, spawnOpt myActorSystem "cpuCounter" (Actors.buttonToggleActor coordinatorActor btnCpu CounterType.Cpu false) [SpawnOption.Dispatcher("akka.actor.synchronized-dispatcher")])
+            (CounterType.Memory, spawnOpt myActorSystem "memoryCounter" (Actors.buttonToggleActor coordinatorActor btnMemory CounterType.Memory false) [SpawnOption.Dispatcher("akka.actor.synchronized-dispatcher")])
+            (CounterType.Disk, spawnOpt myActorSystem "diskCounter" (Actors.buttonToggleActor coordinatorActor btnDisk CounterType.Disk false) [SpawnOption.Dispatcher("akka.actor.synchronized-dispatcher")])
+        ]
 
-        btnAddSeries.Click.Add (fun _ ->
-            let newSeriesName = $"FakeSeries %i{sysChart.Series.Count + 1}"
-            let newSeries = ChartDataHelper.randomSeries newSeriesName None None
-            chartActor <! AddSeries newSeries)
+        // The CPU counter will auto-start at launch
+        toggleActors.[CounterType.Cpu] <! Toggle
+
+        btnCpu.Click.Add (fun _ -> toggleActors.[CounterType.Cpu] <! Toggle)
+        btnMemory.Click.Add (fun _ -> toggleActors.[CounterType.Memory] <! Toggle)
+        btnDisk.Click.Add (fun _ -> toggleActors.[CounterType.Disk] <! Toggle)
 
         form
