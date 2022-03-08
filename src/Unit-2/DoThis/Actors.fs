19718cbaf9b7bae801edcc2837c7a5bfda9dacdc
Mike Cooper <mikecoop@users.noreply.github.com>
2022-03-08 08:44:05 -0500

    Finish Lesson 2

--- src/Unit-2/DoThis/Actors.fs
+++ src/Unit-2/DoThis/Actors.fs
@@ -1,5 +1,6 @@
 ﻿namespace ChartApp
 
+open System
 open System.Collections.Generic
 open System.Windows.Forms.DataVisualization.Charting
 open Akka.Actor
@@ -9,14 +10,25 @@ open Akka.FSharp
 module Messages =
     type InitializeChart =
         | InitializeChart of initialSeries: Map<string, Series>
+        | AddSeries of series: Series
 
 /// Actors used to intialize chart data
 [<AutoOpen>]
 module Actors =
-    let chartingActor (chart: Chart) message =
-        match message with
-        | InitializeChart series ->
-            chart.Series.Clear ()
-            series |> Map.iter (fun k v ->
-                v.Name <- k
-                chart.Series.Add(v))
+    let chartingActor (chart: Chart) (mailbox: Actor<_>) =
+        let rec charting (mapping: Map<string,Series>) = actor {
+            let! message = mailbox.Receive ()
+
+            match message with
+            | InitializeChart series ->
+                chart.Series.Clear ()
+                series |> Map.iter (fun k v ->
+                    v.Name <- k
+                    chart.Series.Add(v))
+                return! charting series
+            | AddSeries series when not <| String.IsNullOrEmpty series.Name && not <| (mapping |> Map.containsKey series.Name) ->
+                let newMapping = mapping.Add (series.Name, series)
+                chart.Series.Add series
+                return! charting newMapping
+        }
+        charting Map.empty<string, Series>
